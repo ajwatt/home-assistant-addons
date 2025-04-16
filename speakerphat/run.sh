fe15f9ee5c3ba578ff1d084ed3acd0561b3b9679
ajwatt <39419418+ajwatt@users.noreply.github.com>
2025-04-16 11:12:11 -0500

    Update run.sh

--- speakerphat/run.sh
+++ speakerphat/run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/with-contenv bashio
 
 apk add --no-cache alsa-utils
-aplay /addons/hassio-spakerphat.old/LGsong-chorus.wav
+#aplay /addons/hassio-spakerphat.old/LGsong-chorus.wav
+aplay "/addons/hassio-spakerphat.old/They Might Be Giants - Apollo 18 - 25 - Fingertips - Please Pass The Milk.flac"
