9441b252c04216cccb5f314193c3bbabc75aa19d
ajwatt <39419418+ajwatt@users.noreply.github.com>
2025-04-21 12:00:08 -0500

    Update run.sh

--- speakerphat/run.sh
+++ speakerphat/run.sh
@@ -4,9 +4,5 @@ STARTSOUND="$(bashio::config 'startup_sound')"
 
 apk add --no-cache alsa-utils
 #aplay /addons/hassio-spakerphat.old/LGsong-chorus.wav
-clear
-echo "aplay:
-"
-aplay -L
-cat /etc/asound.conf
-#aplay /$STARTSOUND
+ls -l /*.wav
+aplay /$STARTSOUND
